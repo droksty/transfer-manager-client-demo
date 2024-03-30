f698007bc0a46641c63e07f628b39805b036e7e3
droksty <droksty@gmail.com>
2024-03-30 12:39:38 +0200

    implement search transfers by date http request

--- src/app/transfers/transfer-list/transfer-list.component.ts
+++ src/app/transfers/transfer-list/transfer-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { TransferService } from '../transfer.service';
+import { Transfer } from '../transfer.interface';
 
 @Component({
   selector: 'app-transfer-list',
@@ -8,11 +9,15 @@ import { TransferService } from '../transfer.service';
   styleUrls: ['./transfer-list.component.css']
 })
 export class TransferListComponent implements OnInit {
+  transfers: Transfer[] = [];
 
-  constructor(private transferService: TransferService) {}
+  constructor(private transferService: TransferService) { }
+
 
   ngOnInit(): void {
-    this.transferService.getTransferList();
+    this.transferService.getTransferList().subscribe(transferList => {
+      this.transfers = transferList;
+    });
   }
 
 }
--- src/app/transfers/transfer.service.ts
+++ src/app/transfers/transfer.service.ts
@@ -1,17 +1,21 @@
 import { HttpClient } from "@angular/common/http";
 import { Transfer } from "./transfer.interface";
 import { Injectable } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Injectable()
 export class TransferService {
   API_URL: string = 'http://localhost:8080/transfers';
-  testdate: string = '?date=2024-05-01'       // TEST ONLY
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private route: ActivatedRoute) {}
+
+
 
   getTransferList() {
-    this.http.get<Transfer[]>(this.API_URL + this.testdate).subscribe(transferList => console.log(transferList));
+    const date = this.route.snapshot.queryParams;
+    return this.http.get<Transfer[]>(this.API_URL, { params: date });
   }
+
 
   insertTransfer(transferDTO: Transfer) {
     this.http.post<Transfer>(this.API_URL, transferDTO).subscribe(data => console.log(data));
