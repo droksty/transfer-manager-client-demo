1a7898c25be0895a44b76c6f1bf97b50c7d6820f
droksty <droksty@gmail.com>
2024-11-28 23:00:12 +0200

    cleanup config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,23 @@
 import { ApplicationConfig, importProvidersFrom } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app.routing.module";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { TransferService } from "./transfers/transfer.service";
-import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
+import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from "@angular/common/http";
 
-export const appConfig: ApplicationConfig = {
-  providers: [
-    importProvidersFrom(BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule),
-    TransferService, provideHttpClient(withInterceptorsFromDi())
-  ]
+function demoInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+  console.log("[OUTGOING REQUEST]");
+  console.log(request);
+  const req = request.clone({
+    headers: request.headers.set('DEMO-HEADER', 'DEMO HEADER') // The original request object is not mutable.
+  });
+  return next(req);
 }
 
-// To ensure a smooth migration, the migration schematic
-// adds more providers to the bootstrapApplication method that necessary/desirable
-
-// Refactor and optimize later
+// To ensure a smooth migration,
+// the migration schematic adds more providers
+// to the bootstrapApplication method than necessary/desirable.
+// Refactor and optimize later?
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom(BrowserModule, AppRoutingModule), provideHttpClient(withInterceptors([demoInterceptor]))
+  ]
+}
